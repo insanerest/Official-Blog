84a416187eb2fc53c4e89fccfc577c130dff9546
Insanerst <157520929+insanerest@users.noreply.github.com>
2025-03-09 17:12:23 -0400

    Delete an unused directory

--- web/frontend/app/routes.ts
+++
@@ -1,8 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home_page.tsx"), // Home route at "/"
-  route("/about", "routes/about_page.tsx"), // About route at "/about"
-  route("/welcome-to-the-blog", "routes/post_routes/welcome-to-the-blog_page.tsx"),
-  route("/contact", "routes/contact_page.tsx")
-] satisfies RouteConfig;
