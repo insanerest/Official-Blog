e5ef36d6c6c4c5229df05e2403d036a3b449f38e
insanerest <aelwa001@hotmail.com>
2025-04-09 09:07:42 -0400

    Removing a some code related to dev

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -10,10 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  base: "/Official-Blog/",
-  server: {
-    host: "0.0.0.0",
-    port: 3002,
-    strictPort: true,
-  },
+  base: "/Official-Blog/"
 });
